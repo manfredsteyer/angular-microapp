7921d245f71a180421170a3b6861921a074d43a4
ManfredSteyer <manfred.steyer@gmx.net>
2018-04-30 21:08:45 +0200

    bootstrap multiple spas

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const AotPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
+const path = require('path');
+const PurifyPlugin = require('@angular-devkit/build-optimizer').PurifyPlugin;
+
+module.exports = {
+  entry: './projects/client-a/src/main.ts',
+  resolve: {
+    mainFields: ['browser', 'module', 'main']
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loaders: ['@ngtools/webpack'] },
+      { test: /\.html$/, loader: 'html-loader',  options: { minimize: true } },
+      {
+        test: /\.css$/,
+        "use": [
+          "style-loader",
+          {
+            "loader": "css-loader",
+            "options": {
+              "sourceMap": false,
+              "import": false
+            }
+          }
+        ]
+      },
+      
+      {
+        test: /\.js$/,
+        loader: '@angular-devkit/build-optimizer/webpack-loader',
+        options: {
+          sourceMap: false
+        }
+      }
+      
+    ]
+  },
+  plugins: [
+    
+    new AotPlugin({
+      skipCodeGeneration: false,
+      tsConfigPath: './projects/client-a/tsconfig.app.json',
+      entryModule: path.resolve(__dirname, './projects/client-a/src/app/app.module#AppModule' )
+    }),
+    
+    new PurifyPlugin()
+    
+  ],
+  output: {
+    path: __dirname + '/dist/shell/client-a',
+    filename: 'main.bundle.js'
+  },
+  mode: 'production'
+};
